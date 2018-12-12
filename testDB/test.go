58d47a5dc9e414499c8004ddb87bb08348361ce7
yiqu zhu <kotomineshiki@outlook.com>
2018-12-12 23:04:27 +0800

    数据库GetPeopleAPi完成
    现在可以通过http://127.0.0.1:8080/people/12
    来访问json文件

--- testDB/test.go
+++ testDB/test.go
@@ -10,13 +10,6 @@ import (
 )
 
 var db *bolt.DB=nil
-func main() {
-	// Open the my.db data file in your current directory.
-	// It will be created if it doesn't exist.
-	Open()
-	Write()
-	Read()
-}
 func Open(){
 	db,err := bolt.Open("my.db", 0600, nil)
 	if err != nil {
@@ -26,61 +19,123 @@ func Open(){
 		log.Fatal(db)
 	}
 }
-func Read(){
-	// 查看数据
-	err := db.View(func(tx *bolt.Tx) error {
+func CreateBucket(){
+	db.Update(func(tx *bolt.Tx) error {
+		// 获取BlockBucket表单
+		tx.CreateBucket([]byte("people"))
+		return nil;
+		})
+}
+func Write() { //只用调用一次的函数，把单个文件写入db
+	db.Update(func(tx *bolt.Tx) error {
+		// 获取Bucket筒
+
+		b:=tx.Bucket([]byte("people"))
+		for i := 1; i < 88; i++ {
+			file, err := os.Open("people/" + strconv.Itoa(i))
+
+			if err != nil {
+				continue//如果不存在，则不读
+				//log.Fatal(err)
+			}
+			temp, err1 := ioutil.ReadAll(file)
+			if err1 != nil {
+				log.Fatal(err)
+			}
+			fmt.Printf("read" + strconv.Itoa(i))
+			// 往表里面存储数据
+			if b != nil {
+				err := b.Put([]byte(strconv.Itoa(i)), []byte(temp))//把数据存入数据库
+				if err != nil {
+					log.Panic("数据存储失败......")
+				}
+			}
+		}
+		return nil
+	})
+}
+func test(){
+	db.View(func(tx *bolt.Tx) error {//查看数据
 
 		// 获取BlockBucket表对象
-		b := tx.Bucket([]byte("People"))
+		b := tx.Bucket([]byte("people"))
 
 		// 往表里面存储数据
 		if b != nil {
 			data := b.Get([]byte("l"))
 			fmt.Printf("123+%s\n",data)
-			data = b.Get([]byte("2"))
+			data = b.Get([]byte("85"))
 			fmt.Printf("1234+%s\n",data)
 		}
 
 		// 返回nil，以便数据库处理相应操作
 		return nil
 	})
-	//更新失败
-	if err != nil {
-		log.Panic(err)
-	}
 }
-func Write() {
-	//读入若干个文件，将他们按照id，json文件内容的格式写入数据库
+func main() {
+	db,err := bolt.Open("my.db", 0600, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if db == nil {
+		log.Fatal(db)
+	}
 
-	// 创建表
-	err := db.Update(func(tx *bolt.Tx) error {
+	db.Update(func(tx *bolt.Tx) error {
 		// 获取BlockBucket表单
-		b := tx.Bucket([]byte("people"))
+		tx.CreateBucket([]byte("people"))
+		return nil;
+	})
 
-		for i := 1; i < 30; i++ {
+	db.Update(func(tx *bolt.Tx) error {
+		// 获取Bucket筒
+
+		b:=tx.Bucket([]byte("people"))
+		for i := 1; i < 88; i++ {
 			file, err := os.Open("people/" + strconv.Itoa(i))
+
 			if err != nil {
-				log.Fatal(err)
+				continue//如果不存在，则不读
+				//log.Fatal(err)
 			}
 			temp, err1 := ioutil.ReadAll(file)
 			if err1 != nil {
 				log.Fatal(err)
 			}
-
+			fmt.Printf("read" + strconv.Itoa(i))
 			// 往表里面存储数据
 			if b != nil {
-				err := b.Put([]byte(strconv.Itoa(i)), []byte(temp))
+				err := b.Put([]byte(strconv.Itoa(i)), []byte(temp))//把数据存入数据库
 				if err != nil {
 					log.Panic("数据存储失败......")
 				}
 			}
 		}
+		return nil
+	})
+	db.View(func(tx *bolt.Tx) error {//查看数据
+
+		// 获取BlockBucket表对象
+		b := tx.Bucket([]byte("people"))
+
+		// 往表里面存储数据
+		if b != nil {
+			data := b.Get([]byte("l"))
+			fmt.Printf("123+%s\n",data)
+			data = b.Get([]byte("85"))
+			fmt.Printf("1234+%s\n",data)
+		}
+
 		// 返回nil，以便数据库处理相应操作
 		return nil
 	})
-	if err != nil {
-		log.Panic(err)
-	}
 
+
+	db.Close()
+
+//	Write()
+//	Read()
+}
+func tee(){
 
 }
