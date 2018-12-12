8f225ac589f5011d4f835c3c9e3981af325394a8
zhujunkai <31615150+zhujunkai@users.noreply.github.com>
2018-12-12 18:17:20 +0800

    zhujunkai

---
+++ vue-test/my-project/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+<div id="app">
+<nav class="navbar navbar-default" role="navigation">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+        <span class="sr-only"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+    </div>
+    <div class="collapse navbar-collapse">
+      <ul class="nav navbar-nav navbar-right">
+            <li><router-link to="/">Home</router-link></li>
+            <li><router-link to="/about">About</router-link></li>
+            <li><router-link to="/documentation">Documentation</router-link></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+<div class="container-fluid" style="margin-top: 60px;">
+  <div class="row center yellow">
+    <div class="jumbotron">
+      <h1>SWAPI</h1>
+      <p class="lead">The Star Wars API</p>
+    </div>
+  </div>
+  <router-view/>
+  <div class="row">
+    <div class="col-lg-12 footer">
+      <a href="#" target="_blank">&copy; Paul Hallett 2018 </a>
+      <span class="pull-right">
+        <a href="#" class="twitter-follow-button" data-show-count="false" data-show-screen-name="false" data-dnt="true">Follow me on twitter</a>
+      </span>
+    </div>
+  </div>
+</div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="css" src="./assets/css/bootstrap.css"></style>
+<style lang="css" src="./assets/css/custom.css"></style>
---
+++ vue-test/my-project/src/components/About.vue
@@ -0,0 +1,45 @@
+<template>
+<div id="About">
+  <div class="row">
+    <div class="col-sm-2 col-lg-2 col-md-2">
+    <h4>Statistics</h4>
+    <p>People: {{people}}</p>
+    <p>Planets: {{planets}}</p>
+    <p>Films: {{films}}</p>
+    <p>Species : {{species}}</p>
+    <p>Vehicles: {{vehicles}}</p>
+    <p>Starships: {{starships}}</p>
+    </div>
+    <div class="col-lg-8 col-md-8 col-sm-8">
+      <vue-markdown :source="content"></vue-markdown>
+    </div>
+    <div class="col-sm-2 col-lg-2 col-md-2">
+        <h4>Donate</h4>
+        <p>With small donations we can keep swapi running for free, please consider throwing us some beer money to say thank you. With every $10 we can keep the site up for another month!</p>
+      <p>We truly appreciate your support</p>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+import VueMarkdown from 'vue-markdown'
+export default {
+  name: 'About',
+  data () {
+    return {
+      people: 87,
+      planets: 61,
+      films: 7,
+      species: 37,
+      vehicles: 39,
+      starships: 37,
+      msg: 'About',
+      content: require('../assets/md/about.md')
+    }
+  },
+  components: {
+    VueMarkdown // 声明组件
+  }
+}
+</script>
