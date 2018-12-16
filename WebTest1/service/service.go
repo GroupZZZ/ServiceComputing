5d46e073cf8396bf964be12a271346e307336d04
yiqu zhu <kotomineshiki@outlook.com>
2018-12-16 22:00:54 +0800

    删除了一些冗余代码

--- WebTest1/service/service.go
+++ WebTest1/service/service.go
@@ -1,10 +1,10 @@
 package service
+
 import (
-	"net/http"
-	"os"
 	"github.com/codegangsta/negroni"
 	"github.com/gorilla/mux"
 	"github.com/unrolled/render"
+	"os"
 )
 func NewServer() *negroni.Negroni {//copy自上课案例
 	formatter := render.New()
@@ -27,10 +27,5 @@ func initRoutes(mx *mux.Router, formatter *render.Render) {
 	mx.HandleFunc("/species/{id}", GetSpeciesApi).Methods("GET")
 	mx.HandleFunc("/starships/{id}", GetStarshipsApi).Methods("GET")
 	//mx.HandleFunc("/people", unknownPage).Methods("GET")
-	mx.HandleFunc("/", indexPage).Methods("GET")
-	mx.HandleFunc("/index", indexPage).Methods("GET")
-	mx.HandleFunc("/calculator", calculatorPage).Methods("GET")
-//	mx.HandleFunc("/", submit).Methods("POST")
-	mx.PathPrefix("/file").Handler(http.StripPrefix("/file/", http.FileServer(http.Dir(webRoot+"/assets/"))))
 
 }
